efda04760b82b43f8ae3a5ba3dd7e1b84cd54ef3
N0T-NixX <alvaromf0209@gmail.com>
2022-05-12 15:30:07 +0200

    Se ha cambiado el boton a enviar y se ha insertado un MB

--- practicaControlVer/practicaControlVer/Form1.Designer.cs
+++ practicaControlVer/practicaControlVer/Form1.Designer.cs
@@ -103,7 +103,7 @@ namespace practicaControlVer
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(99, 38);
             this.button1.TabIndex = 7;
-            this.button1.Text = "Guardar";
+            this.button1.Text = "Enviar";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
--- practicaControlVer/practicaControlVer/Form1.cs
+++ practicaControlVer/practicaControlVer/Form1.cs
@@ -19,7 +19,7 @@ namespace practicaControlVer
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("hola");
+            MessageBox.Show("QUE TAL");
         }
     }
 }
