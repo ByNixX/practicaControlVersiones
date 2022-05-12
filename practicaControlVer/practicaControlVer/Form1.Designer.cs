7ad49673020f75385308451aededd23b1fef665a
ByNixX <bynixxyt@gmail.com>
2022-05-12 20:22:14 +0200

    cambios del fondo, y el codigo del boton

--- practicaControlVer/practicaControlVer/Form1.Designer.cs
+++ practicaControlVer/practicaControlVer/Form1.Designer.cs
@@ -120,7 +120,7 @@ namespace practicaControlVer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Orchid;
+            this.BackColor = System.Drawing.Color.MediumTurquoise;
             this.ClientSize = new System.Drawing.Size(745, 450);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.button1);
--- practicaControlVer/practicaControlVer/Form1.cs
+++ practicaControlVer/practicaControlVer/Form1.cs
@@ -19,7 +19,7 @@ namespace practicaControlVer
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("QUE TAL");
+            MessageBox.Show("QUE TAL, owo");
         }
     }
 }
